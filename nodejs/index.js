7e11372f6572156a30f7e71213ea839b16d8c7a3
Jack Rudenko <i@madappgang.com>
2023-03-31 21:38:33 +1100

    add health modue

--- nodejs/index.js
+++ nodejs/index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('Hello world!')
 })
 
+
+app.get('/health/live', (req, res) => {
+  res.send("I am healthy ..")
+})
+
 app.listen(port, () => {
   console.log(`I am a basic backend running on port ${port}`)
 })
